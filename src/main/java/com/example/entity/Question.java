d5a3531a76e38cfdfbdcdc996c77fbe812d52177
MuZiBin2312 <lijiahe1808255467@163.com>
2025-05-06 22:39:19 +0800

    问题加字段

--- src/main/java/com/example/entity/Question.java
+++ src/main/java/com/example/entity/Question.java
@@ -25,6 +25,15 @@ public class Question implements Serializable {
     /** 审核状态 */
     private String status;
     private String userName;
+    private Integer category;
+
+    public Integer getCategory() {
+        return category;
+    }
+
+    public void setCategory(Integer category) {
+        this.category = category;
+    }
 
     public String getUserName() {
         return userName;
